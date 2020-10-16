911ffb6054e78227d99037293d26d39ca1691ee6
Aitor <aelorza034@ehu.eus>
2020-10-16 20:41:05 +0200

    Aplikazio amaitua baina botoiak nahiz eta funtzionatu erroreak ematen dituzte
    Controller.java eta InformazioKud.java fitxategietako begiratu_informazioa eta bueltatu_hasierara metodotan arazoa sortzen da.

--- src/main/java/ehu/isad/Main.java
+++ src/main/java/ehu/isad/Main.java
@@ -1,11 +1,18 @@
 package ehu.isad;
 
+import ehu.isad.controllers.Controller;
+import ehu.isad.controllers.InformazioKud;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
+/*
 public class Main extends Application {
 
   @Override
@@ -15,10 +22,82 @@ public class Main extends Application {
     primaryStage.setTitle("Hello World");
     primaryStage.setScene(new Scene(root, 300, 275));
     primaryStage.show();
+
   }
 
 
   public static void main(String[] args) {
     launch(args);
   }
+
 }
+*/
+public class Main extends Application{
+  private Text label;
+  private Parent mainUI;
+  private Parent informazioaUI;
+  private Controller mainkud;
+  private InformazioKud informaziokud;
+  private Stage stage;
+
+
+  @Override
+  public void start(Stage primaryStage) throws IOException {
+
+    //primaryStage.setTitle("OpenLibrary APIa aztertzen");
+
+/*
+    comboBox.setOnAction( e -> {
+      Book book = (Book)comboBox.getValue();
+      try {
+        Book liburua = readFromUrl(book.isbn);
+        this.label.setText( liburua.details.title + "\n" +
+                liburua.details.number_of_pages + "\n" +
+                liburua.details.publishers[0] );
+
+      } catch (IOException ioException) {
+        ioException.printStackTrace();
+      }
+    });
+*/
+
+    stage=primaryStage;
+    this.pantailaKargatu();
+    stage.setTitle("Liburuen Informazioa API");
+    stage.setScene(new Scene(mainUI));
+    stage.show();
+  }
+
+  private void pantailaKargatu() throws IOException {
+    FXMLLoader loaderKautotu = new FXMLLoader(getClass().getResource("/sample.fxml"));
+    mainUI = (Parent) loaderKautotu.load();
+    mainkud = loaderKautotu.getController();
+    mainkud.setMainApp(this);
+
+    FXMLLoader loaderinfo=new FXMLLoader(getClass().getResource("/informazio.fxml"));
+    informazioaUI = (Parent) loaderinfo.load();
+    informaziokud = loaderinfo.getController();
+    informaziokud.setMainApp(this);
+
+  }
+
+  public static void main(String[] args) {
+    launch(args);
+  }
+
+
+  public Parent mainErakutsi() {
+    stage.setScene(new Scene(mainUI));
+    stage.show();
+    return stage.getScene().getRoot();
+  }
+  public Parent infoErakutsi(Details xehetasunak, Image irudia) {
+    stage.setScene(new Scene(informazioaUI));
+    stage.show();
+    informaziokud.informazioa_jarri(xehetasunak,irudia);
+    return stage.getScene().getRoot();
+  }
+
+}
+//https://openlibrary.org/api/books?bibkeys=ISBN:1491910399&jscmd=details&format=json"
+
---
+++ src/main/java/ehu/isad/controllers/InformazioKud.java
@@ -0,0 +1,55 @@
+package ehu.isad.controllers;
+
+import ehu.isad.Details;
+import ehu.isad.Main;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+
+import java.util.Arrays;
+
+public class InformazioKud {
+    private Main main_app;
+    public void setMainApp(Main main){
+        this.main_app=main;
+
+    }
+
+    public void informazioa_jarri(Details xehetasunak, Image irudia){
+        izena_label.setText(xehetasunak.title);
+        argitaletxea_label.setText(Arrays.toString(xehetasunak.publishers));
+        orrialde_label.setText(xehetasunak.number_of_pages.toString());
+        irudi_taula.setImage(irudia);
+
+
+
+    }
+    @FXML
+    private AnchorPane bigarren_leihoa;
+    @FXML
+    private Label izena_label;
+
+    @FXML
+    private Button atzera_button;
+
+    @FXML
+    private Label argitaletxea_label;
+
+    @FXML
+    private Label orrialde_label;
+
+    @FXML
+    private ImageView irudi_taula;
+    @FXML
+    void bueltatu_hasierara(ActionEvent event) {
+        atzera_button.getScene().setRoot(main_app.mainErakutsi());
+        //main_app.mainErakutsi();
+
+    }
+
+
+}
